5319976ee92e49ed38b4bc200bbc41ad8c9dfc7c
Nick <dohp.kim@gmail.com>
2021-12-30 23:43:36 +0900

    rubberducking

--- inputstream.jl
+++ inputstream.jl
@@ -9,5 +9,37 @@ for line in eachline(input)
   println(line)
 end
 
-
 close(input)
+
+alice = open("alice.txt")
+
+function extractWords(txt)
+  collected = []
+  for line in eachline(txt)
+    chunk = split(line, " ")
+    append!(collected, chunk)
+  end
+  words = []
+  for letter in collected
+    if letter == ""
+      continue
+    end
+
+    marker = true
+    for oneChar in letter
+      if !isletter(oneChar)
+        marker = false
+        break
+      end
+    end
+    if marker
+      push!(words, lowercase(letter))
+    end
+  end
+  words
+end
+
+result = extractWords(alice)
+println(result)
+
+close(alice)
